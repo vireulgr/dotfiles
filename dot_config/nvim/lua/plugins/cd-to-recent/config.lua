d510533c25385445426297d06fc5bdbe838d3331
George <grreulvi@gmail.com>
2025-11-13 12:38:34 +0300

    Include current directory option if current path is file

--- dot_config/nvim/lua/plugins/cd-to-recent/config.lua
+++ dot_config/nvim/lua/plugins/cd-to-recent/config.lua
@@ -1,8 +1,12 @@
 local homeDirectory = vim.env.HOME
-return {
+local config = {
   predefined_directories = {
-    { name = "Current file directory", path = vim.fn.expand('%:p:h') },
-    { name = "nvim config root", path = homeDirectory .. "/.config/nvim" },
+    { name = "nvim config root", path = homeDirectory .. "\\.config\\nvim" },
   }
 }
+local current_path = vim.fn.expand('%:p')
+if 0 == vim.fn.isdirectory(current_path) then
+  table.insert(config.predefined_directories, 1, { name = "Current file directory", path = vim.fs.dirname(current_path) })
+end
 
+return config
